55a23f8a2f70a177df0badca6ed5d0454466d9d2
nishrat2014 <nishrat2014@gmail.com>
2025-12-15 14:00:15 +0530

    Create test01.py
    just to test

---
+++ test01.py
@@ -0,0 +1,8 @@
+# Import python packages
+import streamlit as st
+#from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.functions import col
+import pandas as pd
+
+st.title("Test App")
+st.write("Hello from Snowflake Streamlit")
