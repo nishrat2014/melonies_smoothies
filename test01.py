23602664305cf5bc37d7a1718f62de2d9dc4dc66
nishrat2014 <nishrat2014@gmail.com>
2025-12-15 14:27:33 +0530

    Update test01.py
    test2

--- test01.py
+++ test01.py
@@ -3,6 +3,8 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 #from snowflake.snowpark.functions import col
 import pandas as pd
+from snowflake.snowpark.functions import col
+
 
 st.title("Test App")
 st.write("Hello from Snowflake Streamlit")
