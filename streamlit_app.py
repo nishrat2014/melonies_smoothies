8ccab9099312b888b95ddc5c2967f0ccd09d3084
nishrat2014 <nishrat2014@gmail.com>
2025-12-16 11:43:45 +0530

    Comment out dataframe display and stop
    Comment out dataframe display and stop execution.

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ schema=st.secrets["snowflake"]["schema"],
 )
 # Get fruit options
 df = pd.read_sql("SELECT FRUIT_NAME, SEARCH_ON FROM FRUIT_OPTIONS", conn)
-st.dataframe(data=df,  use_container_width=True)
-st.stop()
+#st.dataframe(data=df,  use_container_width=True)
+#st.stop()
 
 
 
